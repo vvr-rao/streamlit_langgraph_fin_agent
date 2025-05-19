14b36d12396bc178ab6816bc6ec27ebb7d22bddd
vvr-rao <38475577+vvr-rao@users.noreply.github.com>
2025-05-19 09:15:52 -0400

    Add files via upload

---
+++ cdk_stack.py
@@ -0,0 +1,148 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    aws_ec2 as ec2,
+    aws_ecs as ecs,
+    aws_iam as iam,
+    aws_cognito as cognito,
+    aws_secretsmanager as secretsmanager,
+    aws_cloudfront as cloudfront,
+    aws_cloudfront_origins as origins,
+    aws_elasticloadbalancingv2 as elbv2,
+)
+
+prefix="FinAgent"
+
+
+class CdkStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # VPC for ALB and ECS cluster
+        vpc = ec2.Vpc(
+            self,
+            f"{prefix}Vpc",
+            ip_addresses=ec2.IpAddresses.cidr("10.0.0.0/16"),
+            max_azs=2,
+            vpc_name=f"{prefix}-vpc",
+            nat_gateways=1,
+        )
+
+        ecs_security_group = ec2.SecurityGroup(
+            self,
+            f"{prefix}SecurityGroupECS",
+            vpc=vpc,
+            security_group_name=f"{prefix}-ecs-sg",
+        )
+
+        alb_security_group = ec2.SecurityGroup(
+            self,
+            f"{prefix}SecurityGroupALB",
+            vpc=vpc,
+            security_group_name=f"{prefix}-alb-sg",
+        )
+
+        ecs_security_group.add_ingress_rule(
+            peer=alb_security_group,
+            connection=ec2.Port.tcp(8501),
+            description="ALB traffic",
+        )
+
+        # ECS cluster and service definition
+        cluster = ecs.Cluster(
+            self,
+            f"{prefix}Cluster",
+            enable_fargate_capacity_providers=True,
+            vpc=vpc)
+
+        # ALB to connect to ECS
+        alb = elbv2.ApplicationLoadBalancer(
+            self,
+            f"{prefix}Alb",
+            vpc=vpc,
+            internet_facing=True,
+            load_balancer_name=f"{prefix}",
+            security_group=alb_security_group,
+            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+        )
+
+        fargate_task_definition = ecs.FargateTaskDefinition(
+            self,
+            f"{prefix}WebappTaskDef",
+            memory_limit_mib=512,
+            cpu=256,
+        )
+
+        openai_secret = secretsmanager.Secret.from_secret_name_v2(
+            self,
+            "OPENAI_API_KEY", #precreate this
+            secret_name="OPENAI_API_KEY"
+        )
+        openai_secret.grant_read(fargate_task_definition.task_role)
+
+        # Build Dockerfile from local folder and push to ECR
+        image = ecs.ContainerImage.from_asset('docker_app')
+
+        fargate_task_definition.add_container(
+            f"{prefix}WebContainer",
+            # Use an image from DockerHub
+            image=image,
+            port_mappings=[
+                ecs.PortMapping(
+                    container_port=8501,
+                    protocol=ecs.Protocol.TCP)],
+            logging=ecs.LogDrivers.aws_logs(stream_prefix="WebContainerLogs"),
+            secrets={
+                "OPENAI_API_KEY": ecs.Secret.from_secrets_manager(openai_secret)
+            }
+        )
+
+        service = ecs.FargateService(
+            self,
+            f"{prefix}ECSService",
+            cluster=cluster,
+            task_definition=fargate_task_definition,
+            service_name=f"{prefix}-stl-front",
+            security_groups=[ecs_security_group],
+            vpc_subnets=ec2.SubnetSelection(
+                subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS),
+        )
+
+        # Add ALB as CloudFront Origin
+        origin = origins.LoadBalancerV2Origin(
+            alb,
+            origin_shield_enabled=False,
+            protocol_policy=cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+        )
+
+        cloudfront_distribution = cloudfront.Distribution(
+            self,
+            f"{prefix}CfDist",
+            default_behavior=cloudfront.BehaviorOptions(
+                origin=origin,
+                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                allowed_methods=cloudfront.AllowedMethods.ALLOW_ALL,
+                cache_policy=cloudfront.CachePolicy.CACHING_DISABLED,
+                origin_request_policy=cloudfront.OriginRequestPolicy.ALL_VIEWER,
+            ),
+        )
+
+        # ALB Listener
+        http_listener = alb.add_listener(
+            f"{prefix}HttpListener",
+            port=80,
+            open=True,
+        )
+
+        http_listener.add_targets(
+            f"{prefix}TargetGroup",
+            target_group_name=f"{prefix}-tg",
+            port=8501,
+            priority=1,
+            protocol=elbv2.ApplicationProtocol.HTTP,
+            targets=[service],
+        )
+        
+
+        
